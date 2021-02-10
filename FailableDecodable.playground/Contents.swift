5c199dea432b9677b593da7e825acc5471a12b2d
Mike Mikina <mike@mikina.pl>
2021-02-10 22:24:11 +0100

    Failable decodable Swift playground.

---
+++ FailableDecodable.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+let json = """
+[
+    {
+        "id": 1,
+        "name": "Any vs. AnyObject in Swift",
+        "type": "articles"
+    },
+    {
+        "id": 2,
+        "name": "New Apple silicon",
+        "type": "news"
+    },
+    {
+        "id": 3,
+        "name": "Passing weak self into a closure",
+        "type": "videos"
+    }
+]
+""".data(using: .utf8)!
+
+struct ListElement: Decodable {
+    let id: Int
+    let name: String
+    let type: ElementType
+
+    enum ElementType: String, Decodable {
+        case articles, news
+    }
+}
+
+struct FailableDecodable<T: Decodable>: Decodable {
+    let element: T?
+
+    init(from decoder: Decoder) throws {
+        self.element = try? T(from: decoder)
+    }
+}
+
+struct FailableResult<T: Decodable>: Decodable {
+    let result: Result<T, Error>
+
+    init(from decoder: Decoder) throws {
+        result = Result(catching: { try T(from: decoder) })
+    }
+}
+
+
+let elements = try JSONDecoder().decode([FailableResult<ListElement>].self, from: json).compactMap { try? $0.result.get() }
+print(elements)
